53fcf7a7c58d9fb96c343eb1ff491ff839e39115
Wilson Ramos <wilson.ramos@live.com>
2018-06-25 11:00:54 -0500

    Nuevas cosas

---
+++ Application/src/app/app.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  //interpolacion
+  titulo = 'Web';
+  usuario = {
+    nombre: 'Wilson',
+    apellido: 'Ramos'
+  };
+  arregloNumeros = [5, 4, 3, 2, 1];
+  mostrarTitulo = true;
+  imagenUrl = "https://pm1.narvii.com/6216/4ea9e980e438a251a2ed1f8d518ba3a7a8fe42fb_hq.jpg";
+  imagenWidth = 300;
+
+  html = ` 
+  <h1>HOLA</h1>
+  <p>Hola mundo</p>
+  <img src="${this.imagenUrl}" alt="">
+  `;
+
+  aumentarWidth(evento){
+    console.log('evento', evento);
+    this.imagenWidth += 10;
+
+  }
+  restarWidth(){
+    this.imagenWidth -= 10;
+
+  }
+}
